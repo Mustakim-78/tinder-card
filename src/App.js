881ec960ce3dddad8144e16787b08c44351d3e75
Mustakim-78 <59732290+Mustakim-78@users.noreply.github.com>
2020-12-22 02:48:45 +0530

    tinder card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react';
 import './App.css';
+import TinderCard from 'react-tinder-card'
+import data from './data.js'
 
 function App() {
+
+  const [animals, setAnimals] = useState([])
+  const [userLike, setUserLike] = useState([])
+  const [userDisLike, setUserDisLike] = useState([])
+  
+  console.log(animals);
+  useEffect(() => {
+    console.log(data[0].imgURL);
+    setAnimals(data)
+    //console.log(animals[0].name);
+    return data
+  },[])
+
+
+  function swiped(direction,category){
+    if(direction === 'right')
+      setUserLike(userLike => [...userLike,category])
+    else
+    setUserDisLike(userDisLike => [...userDisLike,category])
+  }
+  function leftScreen(name){
+    console.log(name)
+  }
+  console.log(userLike)
+  console.log(userDisLike)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="app">
+      <h1>Tinder Cards</h1>
+      <div className="container">
+      
+        {animals.map((animal) => (
+            <TinderCard
+            key ={animal.name}
+            className="swipe"
+            //preventSwipe = {['up','down']}
+            onSwipe = {(direction) => swiped(direction,animal.category)}
+            onCardLeftScreen = {() => leftScreen(animal.name)}
+          >
+              <div className="card"
+                style ={{backgroundImage: `url(${animal.imgURL})`}}>
+                <h3>{animal.name}</h3>
+              </div>
+          </TinderCard>
+        ))}
+        </div>
     </div>
   );
 }
